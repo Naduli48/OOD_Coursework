1468f87cb1248694b62fed7e7c8a1c25287a7757
Naduli48 <naduli.20232165@iit.ac.lk>
2025-11-28 20:08:19 +0530

    made changes

--- src/Team.java
+++ src/Team.java
@@ -3,10 +3,22 @@ import java.util.List;
 
 public class Team {
     private String teamID;
-    private List<member> members;
+    private List<Participant> participants;
 
     public Team(String teamID){
         this.teamID = teamID;
-        this.members = new ArrayList<member>();
+        this.participants = new ArrayList<Participant>();
+    }
+
+    public String getTeamID(){
+        return teamID;
+    }
+
+    public List<Participant> getParticipants(){
+        return participants;
+    }
+
+    public void addParticipant(Participant p){
+        participants.add(p);
     }
 }
